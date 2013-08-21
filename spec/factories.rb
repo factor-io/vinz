a60342cbb7d584b2eac11896b7ce2b41d5bc44c8
Alex Parkinson <okalex@gmail.com>
2013-08-20 18:06:10 -0700

    Full organization factory

--- spec/factories.rb
+++ spec/factories.rb
@@ -2,6 +2,46 @@ FactoryGirl.define do
 
   factory :organization do
     name 'Factor.io'
+
+    factory :full_organization do |fo|
+      name 'Full Organization'
+
+      fo.users { |u| [
+        u.association(:user, username: 'user1'),
+        u.association(:user, username: 'user2')
+      ] }
+
+      fo.groups { |g| [
+        g.association(:group, name: 'group1'),
+        g.association(:group, name: 'group2')
+      ] }
+
+      fo.consumers { |c| [
+        c.association(:consumer, name: 'Consumer 1'),
+        c.association(:consumer, name: 'Consumer 2'),
+        c.association(:consumer, name: 'Consumer Both'),
+        c.association(:consumer, name: 'Consumer None')
+      ] }
+
+      fo.config_items { |ci| [
+        ci.association(:config_item, name: 'Item 1'),
+        ci.association(:config_item, name: 'Item 2'),
+        ci.association(:config_item, name: 'Item Both'),
+        ci.association(:config_item, name: 'Item None'),
+      ] }
+
+      after(:build) do |org|
+        org.consumers[0].update_attributes groups: [org.groups.first]
+        org.consumers[1].update_attributes groups: [org.groups.last]
+        org.consumers[2].update_attributes groups: org.groups
+        org.consumers[3].update_attributes groups: []
+
+        org.config_items[0].update_attributes groups: [org.groups.first]
+        org.config_items[1].update_attributes groups: [org.groups.last]
+        org.config_items[2].update_attributes groups: org.groups
+        org.config_items[3].update_attributes groups: []
+      end
+    end
   end
 
   factory :user do
@@ -25,13 +65,11 @@ FactoryGirl.define do
 
   factory :consumer do
     organization
-    groups {[FactoryGirl.create(:group)]}
     name 'consumer'
   end
 
   factory :config_item do
     organization
-    groups {[FactoryGirl.create(:group)]}
     name 'config'
     value 'some value'
   end
